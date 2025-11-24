2fca4cbed9527163a5dcdc6dfefc2e18329298c6
Caue <caueolisilva@gmail.com>
2025-11-24 19:07:11 +0000

    Nos depoimentos estilo comentários do Facebook, faça o seguinte ajuste:

--- src/components/landing/testimonials-section.tsx
+++ src/components/landing/testimonials-section.tsx
@@ -12,7 +12,6 @@ const testimonials = [
     imageHint: 'man portrait',
     reactions: [
       <ThumbsUp key="thumb" className="h-5 w-5 fill-blue-600 text-blue-600" />,
-      <Heart key="heart" className="h-5 w-5 fill-red-600 text-red-600" />
     ]
   },
   {
@@ -23,7 +22,6 @@ const testimonials = [
     imageHint: 'man happy',
     reactions: [
       <ThumbsUp key="thumb" className="h-5 w-5 fill-blue-600 text-blue-600" />,
-      <svg key="fire" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#f97316" stroke="#f97316" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-flame"><path d="M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.38-.5-2-1-3-1.072-2.143-.224-4.054 2-6 .5 2.5 2 4.9 4 6.5 2 1.6 3 3.5 3 5.5a7 7 0 1 1-14 0c0-1.153.433-2.294 1-3a2.5 2.5 0 0 0 2.5 2.5z"/></svg>
     ]
   },
 ];
