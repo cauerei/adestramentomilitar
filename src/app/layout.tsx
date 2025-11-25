bd0ca0789a02b812bc1cd2b973ddf9879b649b0e
Caue <caueolisilva@gmail.com>
2025-11-25 22:03:55 +0000

    remova todos os códigos que instalamos dentro do HEAD da página.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,23 +19,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@500;700&display=swap" rel="stylesheet" />
-        <Script
-          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
-          data-utmify-prevent-xcod-sck
-          data-utmify-prevent-subids
-          async
-          defer
-        ></Script>
-        <Script id="utmify-pixel">
-          {`
-            window.pixelId = "692621cb525a0b39933a08b0";
-            var a = document.createElement("script");
-            a.setAttribute("async", "");
-            a.setAttribute("defer", "");
-            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
-            document.head.appendChild(a);
-          `}
-        </Script>
       </head>
       <body className="font-body antialiased">
         {children}
